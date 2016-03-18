f77ce440b59432bd71e1a35b8fe6e2774f05c8e9
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-03-18 10:11:28 +0100

    add WS2812B illumination

--- SMDHeatBedTeeHeater.ino
+++ SMDHeatBedTeeHeater.ino
@@ -1,8 +1,10 @@
+#include <FastLED.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_SSD1306.h"
 #include "pid_control.h"
+
 
 // TEMP_HIST_SIZE == display.width()
 #define TEMP_HIST_SIZE 128
@@ -10,9 +12,23 @@
 #define GRAPH_HEIGHT 50
 #define GRAPH_UPPER_TEMP 120.0
 #define GRAPH_TEMP_ZERO_OFFSET -15.0
-#define TEMP_OVERSAMPLING 4
-#define PIN_SSR 3
-uint16_t temp_samples_[TEMP_OVERSAMPLING];
+
+// sample temp every 250ms
+#define TEMP_SAMPLING_INTERVALL 250
+// update PID every 4*250ms == 1s
+#define TEMP_OVERSAMPLING_FACTOR 4
+// update dipslay every 2*250ms == 0.5s
+// ASSERT: TEMP_OVERSAMPLING_FACTOR >= DISPLAY_UPDATE_FACTOR
+#define DISPLAY_UPDATE_FACTOR 2
+
+#define HEATER_MOSFET_PIN 3
+#define LED_PIN 2
+#define NUM_LEDS 1
+#define COLOR_ORDER GRB
+#define CHIPSET     WS2812
+#define BRIGHTNESS  220
+CRGB leds[NUM_LEDS];
+uint16_t temp_samples_[TEMP_OVERSAMPLING_FACTOR];
 uint8_t temp_samples_idx_ = 0;
 int8_t temp_history_[TEMP_HIST_SIZE];
 uint8_t temp_history_idx_ = 0;
@@ -57,6 +73,10 @@ void setup() {
   for (uint8_t c=0; c<TEMP_HIST_SIZE; c++) {
     temp_history_[c] = -10; // outside display
   }
+
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness( BRIGHTNESS );
+
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);  
   display.display(); // show splashscreen
@@ -88,6 +108,50 @@ void displayTemp(float temp_target, float temp_current) {
   display.display();
 }
 
+CRGB HeatBedColor( float temperature)
+{
+    CRGB heatcolor;
+
+    // Scale 'heat' up from 0-180 to 0-191,
+    // which can then be easily divided into three
+    // equal 'thirds' of 64 units each.
+    uint8_t t192 = temperature * 1.061;
+
+    // calculate a value that ramps up from
+    // zero to 255 in each 'third' of the scale.
+    uint8_t heatramp = t192 & 0x3F; // 0..63
+    heatramp <<= 2; // scale up to 0..252
+
+    // now figure out which third of the spectrum we're in:
+    if( t192 & 0x80) {
+        // we're in the hottest third
+        heatcolor.r = 255; // full red
+        heatcolor.g = 255; // full green
+        heatcolor.b = heatramp; // ramp up blue
+
+    } else if( t192 & 0x40 ) {
+        // we're in the middle third
+        heatcolor.r = 255; // full red
+        heatcolor.g = heatramp; // ramp up green
+        heatcolor.b = 0; // no blue
+
+    } else {
+        // we're in the coolest third
+        heatcolor.r = heatramp; // ramp up red
+        heatcolor.g = 0; // no green
+        heatcolor.b = 0; // no blue
+    }
+
+    return heatcolor;
+}
+
+void illuminateTemp(float temp_current) {
+  CRGB colour = HeatBedColor(temp_current);
+  for (uint8_t c=0; c < NUM_LEDS; c++)
+    leds[c]=colour;
+  FastLED.show();
+}
+
 void loop() {
   // read the analog in value:
   potiRawValue = analogRead(analogInPin1);            
@@ -99,7 +163,7 @@ void loop() {
   Serial.println(potiRawValue);
 
   //map to range 156 (aka 0°C)  ... 716 (aka 180°C)
-  potiRawValue = potiRawValue * 64 / 117 + 156  //make sure calculation does not exceed 16bit (1024*64 == 2^16)
+  potiRawValue = potiRawValue * 64 / 117 + 156;  //make sure calculation does not exceed 16bit (1024*64 == 2^16)
 
   Serial.print(F("potiRawValue in temp range = "));
   Serial.println(potiRawValue);
@@ -117,29 +181,31 @@ void loop() {
   Serial.print(F("Target : ")); Serial.print(target_temperatureC); Serial.println(F(" degrees C"));
   Serial.print(F("Current: ")); Serial.print(temperatureC); Serial.println(F(" degrees C"));
  
-  displayTemp(target_temperatureC, temperatureC);
+  if (temp_samples_idx_% DISPLAY_UPDATE_FACTOR == 0)
+    displayTemp(target_temperatureC, temperatureC);
+  illuminateTemp(temperatureC);
 
   temp_samples_[temp_samples_idx_] = tempRawValue;
   temp_samples_idx_++;
-  temp_samples_idx_%=TEMP_OVERSAMPLING;
+  temp_samples_idx_%=TEMP_OVERSAMPLING_FACTOR;
 
-  //PID every TEMP_OVERSAMPLING runs
+  //PID every TEMP_OVERSAMPLING_FACTOR runs
   if (temp_samples_idx_ == 0)
   {
     //Calc Average
     tempRawValue=0;
-    for (uint8_t c=0; c<TEMP_OVERSAMPLING; c++)
+    for (uint8_t c=0; c<TEMP_OVERSAMPLING_FACTOR; c++)
       tempRawValue += temp_samples_[c];
-    tempRawValue /= TEMP_OVERSAMPLING;
+    tempRawValue /= TEMP_OVERSAMPLING_FACTOR;
     
     //PID
     pid_setTargetValue(potiRawValue);
     uint16_t stellwert = pid_calc(tempRawValue);
     Serial.print(F("Stellwert : ")); Serial.print(stellwert); Serial.println("");
-    analogWriteFrequency(PIN_SSR, 200); // 200 Hz
-    analogWrite(PIN_SSR, stellwert);
+    analogWriteFrequency(HEATER_MOSFET_PIN, 200); // 200 Hz
+    analogWrite(HEATER_MOSFET_PIN, stellwert);
   }
 
-  delay(500);
+  delay(TEMP_SAMPLING_INTERVALL);
 
 }
